836a7867110672fdfa463e2ab82a360a6861154f
Evgeny <jeni89853449640@gmail.com>
2024-04-13 17:38:00 +0600

    v1.1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 5 };
+
+        Console.WriteLine(numbers[^1]);
+    }
+}
